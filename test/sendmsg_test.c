926ee232da7d870120593f3e812f51d3b0184b2e
drab <23292996+drab-l@users.noreply.github.com>
2023-04-16 16:39:29 +0900

    add sendmsg test #3

--- test/sendmsg_test.c
+++ test/sendmsg_test.c
@@ -1,134 +1,72 @@
 #define _GNU_SOURCE
 #include <sys/socket.h>
-#include <sys/un.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <sys/types.h>
+#include <sys/un.h>
 #include <unistd.h>
-#include <errno.h>
-#include <fcntl.h>
+#include <stdio.h>
+#include <execinfo.h>
 #include <signal.h>
-#include <stddef.h>
 
-#define EXIT_IF_EQ(r_,e_) (((r_) != (e_)) || (exit(EXIT_FAILURE),1))
-#define EXIT_IF_NEG_ONE(e_) EXIT_IF_EQ(-1, e_)
-
-static int get_bind_or_connect_socket(int is_bind, int flag)
+static void recver(int sd)
 {
-    int sock;
-    struct sockaddr_un addr;
-
-    EXIT_IF_NEG_ONE(sock = socket(AF_UNIX, flag|SOCK_CLOEXEC, 0));
-    memset(&addr, 0, sizeof(addr));
-    addr.sun_family = AF_UNIX;
-#define SOCKET_PAH "\0sendmsg_socket"
-    memcpy(addr.sun_path, SOCKET_PAH, sizeof(SOCKET_PAH));
-    EXIT_IF_NEG_ONE(is_bind ?
-        (bind(   sock, (struct sockaddr *)&addr, offsetof(struct sockaddr_un, sun_path) + sizeof(SOCKET_PAH))) :
-        (connect(sock, (struct sockaddr *)&addr, offsetof(struct sockaddr_un, sun_path) + sizeof(SOCKET_PAH)))
-    );
-    return sock;
+    struct msghdr msg = { 0 };
+    pid_t pid[2] = {};
+    struct iovec io = {
+        .iov_base = pid,
+        .iov_len = sizeof(pid)
+    };
+    _Alignas(8) char buf[CMSG_SPACE(sizeof(int)) + CMSG_SPACE(sizeof(struct ucred))];
+    msg.msg_iov = &io;
+    msg.msg_iovlen = 1;
+    msg.msg_control = buf;
+    msg.msg_controllen = sizeof(buf);
+    (void)recvmsg(sd, &msg, 0);
 }
-
-static int get_bind_socket(int flag)
+static void sender(int sd)
 {
-    return get_bind_or_connect_socket(1, flag);
-}
+    struct msghdr msg = { 0 };
+    pid_t pid = getpid();
+    struct iovec io = {
+        .iov_base = &pid,
+        .iov_len = sizeof(pid)
+    };
+    int fd = STDIN_FILENO;
+    struct ucred c = {getpid(), getuid(), getgid()};
+    union {
+        char buf[CMSG_SPACE(sizeof(fd)) + CMSG_SPACE(sizeof(c))];
+        struct cmsghdr align;
+    } u = {};
+    char *buf = u.buf;
 
-static int get_connect_socket(int flag)
-{
-    return get_bind_or_connect_socket(0, flag);
-}
+    msg.msg_iov = &io;
+    msg.msg_iovlen = 1;
+    msg.msg_control = buf;
+    msg.msg_controllen = sizeof(u);
 
-static void init_iov(struct iovec *iov, void *data, size_t len)
-{
-    iov->iov_base = data;
-    iov->iov_len = len;
-}
-
-static void init_msghdr(struct msghdr *msg, struct iovec *iov, size_t iov_len, void *data, size_t data_len, int flags)
-{
-    *msg = (struct msghdr){.msg_iov = NULL};
-    msg->msg_iov = iov;
-    msg->msg_iovlen = iov_len;
-    msg->msg_control = data;
-    msg->msg_controllen = data_len;
-    msg->msg_flags = flags;
-}
-
-static void recvmsg_fd_and_data(int sock, int *fd, void *data, size_t len)
-{
-    struct iovec iov;
-    struct msghdr msg;
-    char buf[CMSG_SPACE(sizeof(*fd))];
-
-    init_iov(&iov, data, len);
-    init_msghdr(&msg, &iov, 1, buf, sizeof(buf), MSG_WAITALL);
-
-    EXIT_IF_NEG_ONE(recvmsg(sock, &msg, 0));
-
-    struct cmsghdr *cmsg = CMSG_FIRSTHDR(&msg);
-    *fd = *((int *)CMSG_DATA(cmsg));
-}
-
-static void sendmsg_fd_and_data(int sock, int fd, void *data, size_t len)
-{
-    struct iovec iov;
-    struct msghdr msg;
-    char buf[CMSG_SPACE(sizeof(fd))];
-    init_iov(&iov, data, len);
-    init_msghdr(&msg, &iov, 1, buf, sizeof(buf), 0);
-
-    struct cmsghdr *cmsg = CMSG_FIRSTHDR(&msg);
-    cmsg->cmsg_len = CMSG_LEN(sizeof(fd));
+    struct cmsghdr *next, *cmsg = CMSG_FIRSTHDR(&msg);
     cmsg->cmsg_level = SOL_SOCKET;
     cmsg->cmsg_type = SCM_RIGHTS;
-    *((int*)CMSG_DATA(cmsg)) = fd;
+    cmsg->cmsg_len = CMSG_LEN(sizeof(fd));
+    memcpy(CMSG_DATA(cmsg), &fd, sizeof(fd));
 
-    EXIT_IF_NEG_ONE(sendmsg(sock, &msg, 0));
-}
-
-static void client_send_fd_and_data(int fd, void *data, size_t size)
-{
-    int sock = get_connect_socket(SOCK_DGRAM);
-    sendmsg_fd_and_data(sock, fd, data, size);
-    close(sock);
-}
-
-static void client_send(void)
-{
-    client_send_fd_and_data(STDIN_FILENO, &(pid_t){getpid()}, sizeof(pid_t));
-}
-
-void sig_child(int num)
-{
-    (void)num;
-    _exit(0);
-}
-
-static void server_recv(void)
-{
-    pid_t pid;
-    int fd, sock = get_bind_socket(SOCK_DGRAM);
-    recvmsg_fd_and_data(sock, &fd, &pid, sizeof(pid));
-    printf("file descriptor %d from pid %d\n", fd, pid);
-    while (splice(fd, NULL, STDOUT_FILENO, NULL, 4096, 0) > 0);
-    close(fd);
+    next = CMSG_NXTHDR(&msg, cmsg);
+    cmsg = next;
+    cmsg->cmsg_level = SOL_SOCKET;
+    cmsg->cmsg_type = SCM_CREDENTIALS;
+    cmsg->cmsg_len = CMSG_LEN(sizeof(c));
+    memcpy(CMSG_DATA(cmsg), &c, sizeof(c));
+    sendmsg(sd, &msg, 0);
 }
 
 int main()
 {
-    switch(fork()) {
-    default :
-        signal(SIGCHLD, sig_child);
-        server_recv();
-        return -1;
-    case 0 :
-        client_send();
-        return 1;
-    case -1 :
-        return 1;
+    int sv[2];
+    socketpair(AF_LOCAL, SOCK_SEQPACKET, 0, sv);
+    if (fork()) {
+        sender(sv[0]);
+    } else {
+        recver(sv[1]);
     }
     return 0;
 }
+
